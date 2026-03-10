345f00d919e19a65676543f8a67bc3c992f5ff70
siammollik <fsmsiam444@gmai,.com>
2026-03-11 00:07:03 +0600

    added modal and spinner

--- home.js
+++ home.js
@@ -1,19 +1,26 @@
+const issuesCount = document.getElementById("issues-count")
+let allIssues = [];
 const loadData = () => {
+    manageSpinner(true)
     fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
         .then((res) => res.json())
         .then((data) => displayData(data.data))
 };
-
-const displayData = (dataAll) => {
-    // console.log(dataAll)
-    const cardContainer = document.getElementById("card-container")
-    cardContainer.innerHTML = " "; 
-    for (let data of dataAll){
-        const newDiv = document.createElement("div");
-        newDiv.innerHTML = `
-            <div class="p-3 space-y-4 rounded-md shadow-lg border-t-5 border-green-500 h-80">
+// modal
+const loadCardDetail = async (id) => {
+    const url = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`
+    const res = await fetch(url);
+    const details = await res.json();
+    displayCardDetails(details.data);
+}
+// display modal
+const displayCardDetails = (data) => {
+    console.log(data)
+    const cardDetailsContainer = document.getElementById("modal-container");
+    cardDetailsContainer.innerHTML = `
+            <div class="p-3 space-y-4 rounded-md shadow-lg h-80">
                     <div class="flex justify-between">
-                        <img src="./assets/Open-Status.png"alt="">
+                        <img class="w-6 h-6" src="${data.status === 'closed' ? './assets/Closed- Status .png' : './assets/Open-Status.png'}"alt="${data.status}">
                         <h3 class="bg-red-200 w-20 text-center rounded-full text-red-600">${data.priority}</h3>
                     </div>
                     <div class="space-y-2">
@@ -26,13 +33,67 @@ const displayData = (dataAll) => {
                     </div>
                     <div class="border-t-2 ">
                         <p>#${data.author}</p>
-                        <p>${data.updatedAt}}</p>
+                        <p>${data.updatedAt}</p>
+
+                    </div>
+                    <div class="modal-action">
+                <form method="dialog">
+                    <!-- if there is a button in form, it will close the modal -->
+                    <button class="btn">Close</button>
+                </form>
+            </div>
+                </div>
+        `
+
+    document.getElementById("my_modal").showModal();
+}
+
+// spinner
+const manageSpinner = (status) => {
+    if (status == true) {
+        document.getElementById("spinner").classList.remove("hidden")
+        document.getElementById("card-container").classList.add("hidden")
+    }
+    else {
+        document.getElementById("spinner").classList.add("hidden")
+        document.getElementById("card-container").classList.remove("hidden")
+    }
+}
+
+
+const displayData = (dataAll) => {
+    // console.log(dataAll)
+    const cardContainer = document.getElementById("card-container")
+    cardContainer.innerHTML = " ";
+    issuesCount.innerText = `${dataAll.length} Issues`
+    for (let data of dataAll) {
+        const newDiv = document.createElement("div");
+        const borderColor = data.status === "open" ? "#00A96E" : data.status === "closed" ? "#A855F7" : "transparent";
+
+        newDiv.style.borderTop = `4px solid ${borderColor}`;
+        newDiv.innerHTML = `
+            <div onclick="loadCardDetail(${data.id})" class="p-3 space-y-4 rounded-md shadow-lg h-80">
+                    <div class="flex justify-between">
+                        <img class="w-6 h-6" src="${data.status === 'closed' ? './assets/Closed- Status .png' : './assets/Open-Status.png'}"alt="${data.status}">
+                        <h3 class="bg-red-200 w-20 text-center rounded-full text-red-600">${data.priority}</h3>
+                    </div>
+                    <div class="space-y-2">
+                        <h3 class="font-bold">${data.title}</h3>
+                        <p>${data.description}</p>
+                        <div class="flex gap-5">
+                            <p class="bg-red-200 w-20 text-center rounded-full text-red-600">badge</p>
+                            <p class="bg-yellow-200 w-20 text-center rounded-full text-yellow-800">badge</p>
+                        </div>
+                    </div>
+                    <div class="border-t-2 ">
+                        <p>#${data.author}</p>
+                        <p>${data.updatedAt}</p>
 
                     </div>
                 </div>
         `
         cardContainer.append(newDiv);
     }
-
+    manageSpinner(false);
 }
 loadData()
